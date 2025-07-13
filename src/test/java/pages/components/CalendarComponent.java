36f01a2ef34dba72070bf25bb5cee744f2fe8430
ayurozhkova <anyanyaf@gmail.com>
2025-07-13 16:20:44 +0500

    delete unused imports

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -2,9 +2,9 @@ package pages.components;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.text;
+
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+
 
 
 public class CalendarComponent {
--- src/test/java/pages/components/ResultTableComponent.java
+++ src/test/java/pages/components/ResultTableComponent.java
@@ -1,6 +1,6 @@
 package pages.components;
 
-import com.codeborne.selenide.Condition;
+
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
